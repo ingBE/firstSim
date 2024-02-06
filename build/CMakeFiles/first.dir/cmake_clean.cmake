3e64f2582af7943c8ebd15903d01ef6b62a902dc
ingBE <chlgyrb111@uos.ac.kr>
2024-02-06 22:20:59 +0900

    ignore build folder

--- build/CMakeFiles/first.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/first"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
