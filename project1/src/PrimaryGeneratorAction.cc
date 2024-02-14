e0b1011c73d88c2f2f0d1f65b2027e6c2240d34b
ingBE <chlgyrb111@uos.ac.kr>
2024-02-14 20:29:15 +0900

    feat: 변경 초기 입자 위치를 env의 한쪽 면 중앙으로 고정

--- project1/src/PrimaryGeneratorAction.cc
+++ project1/src/PrimaryGeneratorAction.cc
@@ -8,7 +8,7 @@
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4SystemOfUnits.hh"
-#include "Randomize.hh"
+//#include "Randomize.hh"
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
 {
@@ -62,9 +62,13 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
             "MyCode0002",JustWarning,msg);
     }
 
-    G4double size = 0.8;
-    G4double x0 = size * envSizeXY * (G4UniformRand()-0.5);
-    G4double y0 = size * envSizeXY * (G4UniformRand()-0.5);
+    //G4double size = 0.8;
+    //G4double x0 = size * envSizeXY * (G4UniformRand()-0.5);
+    //G4double y0 = size * envSizeXY * (G4UniformRand()-0.5);
+    //G4double z0 = -0.5 * envSizeZ;
+    
+    G4double x0 = 0.;
+    G4double y0 = 0.;
     G4double z0 = -0.5 * envSizeZ;
 
     fParticleGun->SetParticlePosition(G4ThreeVector(x0,y0,z0));
